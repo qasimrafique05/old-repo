dd985ad556ebe4b030e7215553c67ec8f03a4988
qasimrafique05 <130813214+qasimrafique05@users.noreply.github.com>
2023-04-15 15:07:00 +0500

    Add files via upload

---
+++ knife.rb
@@ -0,0 +1,21 @@
+# See https://docs.chef.io/workstation/config_rb/ for more information on knife configuration options
+
+
+current_dir = File.dirname(__FILE__)
+
+log_level                :info
+
+log_location             STDOUT
+
+node_name                "qasim"
+
+client_key               "#{current_dir}/qasim.pem"
+
+chef_server_url          "https://api.chef.io/organizations/qrafique"
+
+validation_client_name 	  "qrafique-validator"
+validation_key           "#{current_dir}/qrafique-validator.pem"
+chef_server_url          "https://api.chef.io/organizations/qrafique"
+cache_type 'BasicFile'
+cache_options( :path => "#{ENV['HOME']}/.chef/checksums" )
+cookbook_path            ["#{current_dir}/../cookbooks"]
